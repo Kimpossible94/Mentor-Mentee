3c8b4cb004e05ed5b319bc2b9191e28b55734f8c
Kimpossible <Kimpossible94@naver.com>
2021-12-18 10:57:12 +0900

    apply .gitignore

---
+++ src/test/java/com/mm/kim/mentormentee/MentorMenteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.kim.mentormentee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorMenteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
